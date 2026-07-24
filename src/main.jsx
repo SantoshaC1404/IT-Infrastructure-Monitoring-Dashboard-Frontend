bb26b3969b17a23d7fde255c78d9df49a424e864
SantoshaC1404 <santosha1404@gmail.com>
2026-07-24 21:21:44 +0530

    "toast-utils updated"

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+
+    <Toaster
+      position="top-right"
+      reverseOrder={false}
+      gutter={12}
+      toastOptions={{
+        duration: 4000,
+
+        style: {
+          borderRadius: "12px",
+          background: "#fff",
+          color: "#111827",
+        },
+
+        success: {
+          iconTheme: {
+            primary: "#16a34a",
+            secondary: "#fff",
+          },
+        },
+
+        error: {
+          iconTheme: {
+            primary: "#dc2626",
+            secondary: "#fff",
+          },
+        },
+      }}
+    />
   </StrictMode>,
-)
+);
