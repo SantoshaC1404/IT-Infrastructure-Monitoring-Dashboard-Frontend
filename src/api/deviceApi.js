58982035564b370fe0646fe08ab23b96b03f0979
SantoshaC1404 <santosha1404@gmail.com>
2026-07-27 20:30:40 +0530

    "apis updated"

--- src/api/deviceApi.js
+++ src/api/deviceApi.js
@@ -2,18 +2,24 @@ import api from "./axios";
 
 const BASE_URL = "/devices";
 
-export const getAll = () => api.get(BASE_URL);
+export const getAll = () => {
+  return api.get(BASE_URL);
+};
 
-export const getById = (id) =>
-    api.get(`${BASE_URL}/${id}`);
+export const create = (payload) => {
+  return api.post(BASE_URL, payload);
+};
 
-export const create = (payload) =>
-    api.post(BASE_URL, payload);
+export const update = (id, payload) => {
+  return api.put(`${BASE_URL}/${id}`, payload);
+};
 
-export const update = (id, payload) =>
-    api.put(`${BASE_URL}/${id}`, payload);
-
-const deleteDevice = (id) =>
-    api.delete(`${BASE_URL}/${id}`);
+export const deleteDevice = (id) => {
+  return api.delete(`${BASE_URL}/${id}`);
+};
 
 export { deleteDevice as delete };
+
+export const testConnection = (payload) => {
+  return api.post(`${BASE_URL}/test-connection`, payload);
+};
