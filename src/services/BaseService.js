34c40ad6a1c9a87f5316fd3bb3ad353b306564a7
SantoshaC1404 <santosha1404@gmail.com>
2026-07-27 20:34:12 +0530

    "services updated"

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -1,29 +1,73 @@
 export default class BaseService {
-    constructor(api) {
-        this.api = api;
+  constructor(api) {
+    this.api = api;
+  }
+
+  // GET ALL
+  async getAll() {
+    try {
+      const response = await this.api.getAll();
+      return response.data;
+    } catch (error) {
+      throw this.handleError(error);
+    }
+  }
+
+  // GET BY ID
+  async getById(id) {
+    try {
+      const response = await this.api.getById(id);
+      return response.data;
+    } catch (error) {
+      throw this.handleError(error);
+    }
+  }
+
+  // CREATE
+  async create(payload) {
+    try {
+      const response = await this.api.create(payload);
+      return response.data;
+    } catch (error) {
+      throw this.handleError(error);
+    }
+  }
+
+  // UPDATE
+  async update(id, payload) {
+    try {
+      const response = await this.api.update(id, payload);
+      return response.data;
+    } catch (error) {
+      throw this.handleError(error);
+    }
+  }
+
+  // DELETE BY ID
+  async delete(id) {
+    try {
+      await this.api.delete(id);
+    } catch (error) {
+      throw this.handleError(error);
+    }
+  }
+
+  // ERROR HANDLING
+  handleError(error) {
+    if (error.response) {
+      throw new Error(
+        error.response.data?.message ||
+          error.response.data?.detail ||
+          "Request failed.",
+      );
     }
 
-    async getAll() {
-        const response = await this.api.getAll();
-        return response.data;
+    if (error.request) {
+      throw new Error(
+        "Unable to connect to the server. Please check your network.",
+      );
     }
 
-    async getById(id) {
-        const response = await this.api.getById(id);
-        return response.data;
-    }
-
-    async create(payload) {
-        const response = await this.api.create(payload);
-        return response.data;
-    }
-
-    async update(id, payload) {
-        const response = await this.api.update(id, payload);
-        return response.data;
-    }
-
-    async delete(id) {
-        await this.api.delete(id);
-    }
+    throw new Error(error.message || "Unexpected error.");
+  }
 }
