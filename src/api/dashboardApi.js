3af96a18d25477bdc1dadd3d647c239dae43d3f1
SantoshaC1404 <santosha1404@gmail.com>
2026-08-03 19:47:55 +0530

    "apis updated"

--- src/api/dashboardApi.js
+++ src/api/dashboardApi.js
@@ -5,3 +5,7 @@ const BASE_URL = "/dashboard";
 export const getSummary = () => {
   return api.get(`${BASE_URL}/summary`);
 };
+
+export const getDevices = () => {
+  return api.get(`${BASE_URL}/devices`);
+};
