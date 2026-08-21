7a899bb5624f9e3a423f2efa5942ffda61c2b36f
SantoshaC1404 <santosha1404@gmail.com>
2026-08-21 17:25:53 +0530

    app routes updated

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,19 +1,76 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import Login from "../pages/auth/Login";
+import Login from "../features/auth/pages/Login";
+import Register from "../features/auth/pages/Register";
+
 import Dashboard from "../features/dashboard/pages/Dashboard";
 import Device from "../features/devices/pages/Devices";
+import CriticalDevices from "../features/devices/pages/CriticalDevices";
+import Users from "../features/users/pages/Users";
+
+import ProtectedRoute from "./ProtectedRoute";
+import GuestRoute from "./GuestRoute";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
 
-      <Route path="/login" element={<Login />} />
+      <Route
+        path="/login"
+        element={
+          <GuestRoute>
+            <Login />
+          </GuestRoute>
+        }
+      />
 
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/register"
+        element={
+          <GuestRoute>
+            <Register />
+          </GuestRoute>
+        }
+      />
 
-      <Route path="/devices" element={<Device />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/devices"
+        element={
+          <ProtectedRoute>
+            <Device />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/devices/critical"
+        element={
+          <ProtectedRoute>
+            <CriticalDevices />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/users"
+        element={
+          <ProtectedRoute>
+            <Users />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="*" element={<Navigate to="/dashboard" replace />} />
     </Routes>
   );
 };
