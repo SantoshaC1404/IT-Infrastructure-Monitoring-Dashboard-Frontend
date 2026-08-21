1df8e8d42470236a0245fa38faaa48b35bc72e35
SantoshaC1404 <santosha1404@gmail.com>
2026-08-21 17:24:46 +0530

    app providers updated

--- src/providers/AppProviders.jsx
+++ src/providers/AppProviders.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
+import { AuthProvider } from "../contexts/AuthContext";
+import { ThresholdsProvider } from "../features/monitoring/contexts/ThresholdsContext";
+
 const AppProviders = ({ children }) => {
   return (
     <BrowserRouter>
-      <Toaster position="top-right" />
-      {children}
+      <Toaster position="top-right" reverseOrder={false} />
+
+      <AuthProvider>
+        <ThresholdsProvider>{children}</ThresholdsProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
