6aa36d6d42a435f434fcccdc7691b793094000d7
SantoshaC1404 <santosha1404@gmail.com>
2026-08-21 17:31:19 +0530

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import AppRoutes from "./routes/AppRoutes";
-import { Toaster } from "react-hot-toast";
 
 function App() {
-  return (
-    <>
-      <Toaster position="top-right" reverseOrder={false} />
-
-      <AppRoutes />
-    </>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
