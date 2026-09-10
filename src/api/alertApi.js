a936fb5c64b30b7e765349f9d2540f782482fa2b
SantoshaC1404 <santosha1404@gmail.com>
2026-09-10 21:52:15 +0530

    apis updated

--- src/api/alertApi.js
+++ src/api/alertApi.js
@@ -2,36 +2,34 @@ import api from "./axios";
 
 const BASE_URL = "/alerts";
 
-// export const getAll = (params) => {
-//   return api.get(BASE_URL, {
-//     params,
-//   });
-// };
-
-// export const getOpen = () => {
-//   return api.get(`${BASE_URL}/open`);
-// };
-
-// export const acknowledge = (id) => {
-//   return api.post(`${BASE_URL}/${id}/acknowledge`);
-// };
-
-// export const resolve = (id) => {
-//   return api.post(`${BASE_URL}/${id}/resolve`);
-// };
-
+/**
+ * Get latest alerts
+ */
 export const getAlerts = (limit = 100) => {
-  return api.get(`/alerts?limit=${limit}`);
+  return api.get(BASE_URL, {
+    params: {
+      limit,
+    },
+  });
 };
 
+/**
+ * Get unresolved/open alerts
+ */
 export const getOpenAlerts = () => {
-  return api.get("/alerts/open");
+  return api.get(`${BASE_URL}/open`);
 };
 
+/**
+ * Acknowledge an alert
+ */
 export const acknowledgeAlert = (alertId) => {
-  return api.post(`/alerts/${alertId}/acknowledge`);
+  return api.post(`${BASE_URL}/${alertId}/acknowledge`);
 };
 
+/**
+ * Resolve an alert
+ */
 export const resolveAlert = (alertId) => {
-  return api.post(`/alerts/${alertId}/resolve`);
+  return api.post(`${BASE_URL}/${alertId}/resolve`);
 };
